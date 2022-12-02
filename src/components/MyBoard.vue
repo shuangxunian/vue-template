e436e868b7a8ac01f5f5b61691b83b6d99f9a0f8
yuekin.wang <yuelin.wang@xbongbong.com>
2022-12-02 16:27:24 +0800

    first commit

---
+++ src/components/MyBoard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="board-main">
+    111
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+.board-main {
+  background-color: red;
+}
+</style>
